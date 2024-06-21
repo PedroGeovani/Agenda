0ddf0ec02630d5cb1ab03cb520532e01d3e6c941
Pedro Geovani <pedro.geovanisc@gmail.com>
2024-06-21 09:51:59 -0300

    Projeto iniciado

---
+++ Calculadora/src/components/buttons/ButtonUserAdd.vue
@@ -0,0 +1,15 @@
+<template lang="ts">
+   <p> {{textButton}} </p>
+</template>
+
+<script>
+
+export default {
+    name : 'ButtonUserAdd',
+    data(){
+        return{
+            textButton : "Este é um botão",
+        }
+    }//data
+}
+</script>
