05d168e2ff57dafa411b738f74d3c6e277939259
Pedro Geovani <pedro.geovanisc@gmail.com>
2024-06-21 09:51:59 -0300

    Projeto iniciado

---
+++ Calculadora/src/App.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+
+export default{
+  name : "App",
+  data(){
+    return{
+      title : "Calculadora: Projeto iniciado"
+    }
+  },//data
+
+}
+</script>
+
+<template> 
+
+  <h1> {{title}} </h1>
+  
+</template>
+
+<style scoped>
+
+</style>
