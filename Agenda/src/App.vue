dc4c86d1357f9f7c48bd38c153ffb0be127c7d25
Pedro Geovani <pedro.geovanisc@gmail.com>
2024-07-09 23:10:44 -0300

    first version of the agenda completed

---
+++ Agenda/src/App.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+
+import Navigation from '@/components/navigation/Navigation.vue';
+export default {
+  components: {
+    Navigation,
+  }
+}
+
+</script>
+
+<template>
+  <div>
+    <Navigation />
+    <router-view> </router-view>
+  </div>
+</template>
